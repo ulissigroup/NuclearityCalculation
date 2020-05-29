72b57b6dcca2104eb559325726a603f93043e70e
unnattisharma <57771838+unnattisharma@users.noreply.github.com>
2020-05-28 20:30:56 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from distutils.command.install import INSTALL_SCHEMES
+
+
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+setup(name='drl_nuclearity_calc',
+      version='0.0.1',
+      description='Module for surface nuclearity calculation',
+      url='https://github.com/ulissigroup/NuclearityCalculation',
+      author='Unnatti Sharma, Zack Ulissi',
+      author_email='zulissi@andrew.cmu.edu',
+      license='GPL',
+      platforms=[],
+      packages=find_packages(),
+      scripts=[],
+      include_package_data=False,
+      install_requires=[
+			'ase>=3.19.1',
+			'numpy',
+			'matplotlib',
+            'pymatgen',
+            'pandas',
+			'gaspy @ git+https://github.com/ulissilab/GASpy.git'],
+      long_description='''Module for surface nuclearity calculation for any slab''',)
