a4a6026ac8aa1233f62678fb578fb3d4dae508f7
unnattisharma <57771838+unnattisharma@users.noreply.github.com>
2020-11-22 22:06:39 -0500

    Update surface_nuclearity_calculator.py

--- examples/surface_nuclearity_calculator.py
+++ examples/surface_nuclearity_calculator.py
@@ -23,7 +23,7 @@ import tqdm
 def get_initial_aflow_results(nspecies=2,enthalpy_formation_atom=-0.1):
     result = search(batch_size=10000
                    ).filter((K.nspecies==nspecies)&(K.enthalpy_formation_atom<enthalpy_formation_atom)
-                           ).select(K.species)
+                           ).select(K.species,K.stoich,K.compound,K.auid)
     return list(tqdm.tqdm(result))
 
 def bulk_nuclearity(b,actives):
