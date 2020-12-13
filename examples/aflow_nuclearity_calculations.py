34aea5b0afc08d1c6fdef50fd500341cdccc3a99
zack <zulissi@gmail.com>
2020-12-13 21:26:58 +0000

    add caching to the intermediate results

--- examples/aflow_nuclearity_calculations.py
+++ examples/aflow_nuclearity_calculations.py
@@ -3,17 +3,17 @@ import dask.bag as db
 from joblib import Memory
 from dask.distributed import Client
 import functools
-from surface_nuclearity_calculator import bulk_nuclearity, select_bimetallic,get_initial_aflow_results
+from surface_nuclearity_calculator import bulk_nuclearity, select_bimetallic,get_initial_aflow_results, aflow_object_to_atoms
 from surface_nuclearity_calculator import slab_enumeration,surface_nuclearity_calculator
-#from dask.cache import Cache
 from dask.distributed import progress
 from pymatgen.io.ase import AseAtomsAdaptor
+import dask
 
 # Set the up a kube dask cluster
 cluster = KubeCluster.from_yaml('worker-spec.yml')
 
 # Adapt seems to be having problems, used fixed scaling
-cluster.scale(8)
+cluster.scale(40)
 client = Client(cluster)
 
 ### Code to upload surface_nuclearity code to every worker as they start/restart
@@ -60,25 +60,36 @@ def slab_nuclearity(master,actives):
             'slab_atoms':slab_atoms,
             'nuclearity':nuclearity}
 
+# Set up the cache directory - this will also be the local directory on each worker that will store cached results
 location = './cachedir'
 memory = Memory(location,verbose=1)
-get_initial_aflow_results = memory.cache(get_initial_aflow_results)
+
+# Define the active and inactive elements to consider
 actives = set(['Pd', 'Pt', 'Rh', 'Ru', 'Ag', 'Ir'])
 hosts = set(['Zn', 'Cd', 'Ga', 'Al', 'In'])
 
-print("getting intital results")
+# Gather the aflowlib bulk structures
+get_initial_aflow_results = memory.cache(get_initial_aflow_results)
 all_aflow_binaries = get_initial_aflow_results(enthalpy_formation_atom=-0.1)
 print("Total aflow binaries found = ",len(all_aflow_binaries))
-active_inactive_aflow_binaries = list(filter(lambda r: select_bimetallic(r,actives,hosts), all_aflow_binaries))[:100]
-print("Number of bimetallics found = ",len(active_inactive_aflow_binaries))
-active_inactive_aflow_binaries_bag = db.from_sequence(active_inactive_aflow_binaries)
-print("structure generation")
-all_structures = active_inactive_aflow_binaries_bag.map(lambda b: AseAtomsAdaptor.get_structure(b.atoms(pattern='CONTCAR.relax*', quippy=False, keywords=None, calculator=None)))
-print("slab enumeration")
-all_slabs_list = all_structures.map(slab_enumeration, active_inactive_aflow_binaries_bag)
-all_slabs_list = all_slabs_list.flatten().repartition(npartitions=1000)
-print("nuclearity calculations")
-nuclearity_results = all_slabs_list.map(slab_nuclearity,actives).persist()
 
-progress(nuclearity_results)
+# Find all of the active/inactive combinations
+active_inactive_aflow_binaries = list(filter(lambda r: select_bimetallic(r,actives,hosts), 
+                                             all_aflow_binaries))[:700] # There is a problematic AlPd structure that stops this from going over 700
+print("Number of active/inactive bimetallic structures found = ",len(active_inactive_aflow_binaries))
 
+# Load all of the bulks into a dask bag, and get the atoms objects from aflowlib
+active_inactive_aflow_binaries_bag = db.from_sequence(active_inactive_aflow_binaries, 
+                                                      npartitions=len(active_inactive_aflow_binaries))
+all_structures = active_inactive_aflow_binaries_bag.map(memory.cache(aflow_object_to_atoms))
+
+# Enumerate all of the slabs, and repartition into 10k chunks of surfaces to work on
+all_slabs_list = all_structures.map(memory.cache(slab_enumeration), 
+                                    active_inactive_aflow_binaries_bag)
+all_slabs_list = all_slabs_list.flatten().repartition(npartitions=10000)
+
+# Run the nuclearity calculation on all of the slabs
+nuclearity_results = all_slabs_list.map(memory.cache(slab_nuclearity),actives)
+
+# Compute!
+nuclearity_results = nuclearity_results.compute()
