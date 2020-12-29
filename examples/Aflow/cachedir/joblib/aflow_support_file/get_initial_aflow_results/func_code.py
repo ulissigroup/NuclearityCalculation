af7231e22808170f8caf405d770970051704a2fd
unnattisharma <57771838+unnattisharma@users.noreply.github.com>
2020-12-29 14:22:04 -0500

    Delete func_code.py

--- examples/Aflow/cachedir/joblib/aflow_support_file/get_initial_aflow_results/func_code.py
+++
@@ -1,6 +0,0 @@
-# first line: 28
-def get_initial_aflow_results(nspecies=2,enthalpy_formation_atom=-0.1):
-    result = search(batch_size=10000
-                   ).filter((K.nspecies==nspecies)&(K.enthalpy_formation_atom<enthalpy_formation_atom)
-                           ).select(K.species,K.stoich,K.compound,K.auid)
-    return list(tqdm.tqdm(result))
